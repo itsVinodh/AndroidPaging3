403a93fd968a6a2b2bc7490da08d06c47a097b4e
itsVinodh <vinodh.mobiledev@gmail.com>
2021-01-02 17:04:18 +0530

    Editing MainActivity.kt

--- app/src/main/java/com/example/daggermvvmsample/MainActivity.kt
+++ app/src/main/java/com/example/daggermvvmsample/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.daggermvvmsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
-import com.example.daggermvvmsample.databinding.ActivityMainBinding
 import com.example.daggermvvmsample.adapter.MovieAdapter
 import com.example.daggermvvmsample.adapter.MovieLoadStateAdapter
+import com.example.daggermvvmsample.databinding.ActivityMainBinding
 import com.example.daggermvvmsample.ui.MovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -28,14 +26,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         //adding comment2
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val adapter = MovieAdapter()
         binding.apply {
             rclrViewMovie.setHasFixedSize(true)
             rclrViewMovie.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = MovieLoadStateAdapter{adapter.retry()},
-                footer = MovieLoadStateAdapter{adapter.retry()}
+                header = MovieLoadStateAdapter { adapter.retry() },
+                footer = MovieLoadStateAdapter { adapter.retry() }
             )
         }
 
